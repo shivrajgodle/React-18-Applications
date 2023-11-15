d153a02fa19395af7bd55f16b2683703e9a959ff
SHIVRAJ <83442544+shivrajgodle@users.noreply.github.com>
2023-11-15 14:51:44 +0530

    added logic to Send the Props data from parent component to child component

--- pizza-app/src/index.js
+++ pizza-app/src/index.js
@@ -70,27 +70,30 @@ function Menu(){
   return (
     <div className="menu">
       <h2>Our Menu</h2>;
-        <Pizza/>
-        <Pizza/>
-        <Pizza/>
+        <Pizza  name= "Pizza Prosciutto" ingredients= "Tomato, mozarella, ham, aragula, and burrata cheese"
+         price= "18" photoName= "pizzas/prosciutto.jpg"/>
+
+         <Pizza  name="Pizza Salamino" ingredients="Tomato, mozarella, and pepperoni" price="15" photoName="pizzas/salamino.jpg" />
     </div>
   );
-  
+}
+
+function Pizza(props){
+  return (
+    <div>
+      <img src={props.photoName} alt={props.name} />
+      <h3>{props.name}</h3>
+      <p>{props.ingredients}</p>
+      <span>{props.price}</span>
+    </div>
+  )
 }
 
 function Footer(){
   return <div>Open Today</div>;
 }
 
-function Pizza(){
-  return (
-    <div>
-      <img src="pizzas/prosciutto.jpg" alt="Pizza" />
-      <h3>Pizza title</h3>
-      <div>Pizza description</div>
-    </div>
-  )
-}
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(

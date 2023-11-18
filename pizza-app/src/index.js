aaa48ff8ec65dfa1b9d5640011b1337f1fd2f01d
SHIVRAJ <83442544+shivrajgodle@users.noreply.github.com>
2023-11-18 20:53:37 +0530

    added React Fragment and apply classes dynamically for JSX elements

--- pizza-app/src/index.js
+++ pizza-app/src/index.js
@@ -90,14 +90,13 @@ function Menu(){
 }
 
 function Pizza(props){
-  if(props.pizzaObj.soldOut) return null;
   return (
-    <li className="pizza">
+    <li className={`pizza ${props.pizzaObj.soldOut ? 'sold-out' : ''}`}>
       <img src={props.pizzaObj.photoName} alt={props.pizzaObj.name} />
       <div>
         <h3>{props.pizzaObj.name}</h3>
         <p>{props.pizzaObj.ingredients}</p>
-        <span>{props.pizzaObj.price}</span>
+        <span>{props.pizzaObj.soldOut ? 'SOLD OUT' : props.pizzaObj.price}</span>
       </div>
     </li>
     )
